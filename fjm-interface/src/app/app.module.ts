4d9ea27310349258a15deec4f965a951212e873e
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2019-01-01 02:51:58 +0100

    components created
    components for list of jobs display, job details display and general information about the api is created

--- fjm-interface/src/app/app.module.ts
+++ fjm-interface/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { JobDetailComponent } from './job-detail/job-detail.component';
+import { JobListComponent } from './job-list/job-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    JobDetailComponent,
+    JobListComponent
   ],
   imports: [
     BrowserModule
