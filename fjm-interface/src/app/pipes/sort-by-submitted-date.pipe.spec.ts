5c2f49914880c1f481b5d62b3d3733209bcadc89
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2019-01-10 10:48:59 +0100

    services folder created, pipes folder created

---
+++ fjm-interface/src/app/pipes/sort-by-submitted-date.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { SortBySubmittedDatePipe } from './sort-by-submitted-date.pipe';
+
+// describe('SortBySubmittedDatePipe', () => {
+//   it('create an instance', () => {
+//     const pipe = new SortBySubmittedDatePipe();
+//     expect(pipe).toBeTruthy();
+//   });
+// });
